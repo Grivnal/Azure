df0c654138bdc64617cad86782243ed0f3154e86
Grivnal <153536388+Grivnal@users.noreply.github.com>
2025-07-06 16:52:37 +0000

    Final

---
+++ pv311_web_api/pv311_web_api.BLL/DTOs/Role/UpdateRoleDto.cs
@@ -0,0 +1,8 @@
+namespace pv311_web_api.BLL.DTOs.Role
+{
+    public class UpdateRoleDto
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
